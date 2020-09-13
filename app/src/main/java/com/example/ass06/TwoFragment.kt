be7c76a676cbe26f9e96bed7d562f28a975052d8
kasama111 <kasama_ch@kkumail.com>
2020-09-13 19:40:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ass06/TwoFragment.kt
@@ -0,0 +1,40 @@
+package com.example.ass06
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_two.view.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [TwoFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class TwoFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_one,container, false)
+        view.btnClickFragTwo.setOnClickListener (){
+            var fragment: Fragment? = null
+            fragment = OneFragment()
+            replaceFragment(fragment)
+        }
+        return view
+    }
+    fun replaceFragment(someFragment: Fragment) {
+        val transaction = fragmentManager!!.beginTransaction()
+        transaction.replace(R.id.framelayout, someFragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+}
